fc1c9439c91d3a5f4ac4f8006b3d3c570f44c27d
n1yatsh1n <116572958+n1yatsh1n@users.noreply.github.com>
2023-05-15 12:17:32 +0600

    Delete SpiderAnMuha.py

--- SpiderAnMuha.py
+++
@@ -1,5 +0,0 @@
-f = open('test.txt')
-p = f.readline().split(' ')
-s = f.readline().split(' ')
-m = f.readline().split(' ')
-r = ((int(s[0])-int(m[0]))**2 + (int(s[1])-int(m[1]))**2 + (int(s[2])-int(m[2]))**2) ** 0.5
